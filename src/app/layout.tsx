806d9a97e815ad3c27aefc03d76fe72809c59f0f
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-06-01 17:34:50 -0600

    fix: typo when I named the file FirebaseAuthProviders, there was an additional s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import globalBackground from "../../public/generalBackground.jpg";
 
 import ApolloGraphQLProvider from "@/providers/ApolloGraphQLProvider";
 import StoreProvider from './../providers/StoreProvider'
-import FirebaseAuthProvider from "@/providers/FirebaseAuthProviders";
+import FirebaseAuthProvider from "@/providers/FirebaseAuthProvider";
 import ProtectedRouteGuard from "@/providers/ProtectRouteGuard";
 
 export const metadata: Metadata = {
