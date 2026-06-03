6c643062e7ee8d012919b081313a29ec153a9987
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-06-03 15:56:05 -0600

    feat: show only the clinics where user is active

--- src/components/ClinicCardContainer.tsx
+++ src/components/ClinicCardContainer.tsx
@@ -4,15 +4,18 @@ import { useAppSelector } from "@/lib/store";
 
 import ClinicCard from "./ClinicCard";
 
+import { Status } from '@/__generated__/graphql';
+
 export default function ClinicCardContainer() {
   const { clinicMemberships } = useAppSelector((state) => state.auth.user);
+  const activeClinicMemberships = clinicMemberships.filter((clinic) => clinic.status === "ACTIVE")
 
   return (
     <div className="w-full flex flex-col items-center gap-2 mb-8">
       <p className="w-full text-left font-sans font-bold text-xs  uppercase tracking-wider text-slate-400 pl-1 mb-2">
         Select a clinic to open
       </p>
-      {clinicMemberships.map((membership) => {
+      {activeClinicMemberships.map((membership) => {
         return <ClinicCard key={membership.clinicId} membership={membership} />;
       })}
     </div>
