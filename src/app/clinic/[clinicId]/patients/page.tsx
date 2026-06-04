9de0579dcd906795fff82f6aae22b8028ff70dd9
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-06-04 15:32:19 -0600

    feat: route protection depending on user role (#6)
    * feat: add initial page setup for appointments and patients
    
    * feat: create appointments and patients route
    
    * feat: withRoleProtection component for private routes

---
+++ src/app/clinic/[clinicId]/patients/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { withRoleProtection } from "@/components/common/withRoleProtection";
+import { use } from "react";
+
+type Props = {
+  params: Promise<{ clinicId: string }>;
+};
+
+function PatientsPage({ params }: Props) {
+  const { clinicId } = use(params);
+
+  return (
+    <div className="relative z-50 min-h-screen bg-white p-8 text-black">
+      <h1 className="text-2xl font-bold">Clinic ID: {clinicId}</h1>
+      <p className="mt-2 text-lg">Patient List</p>
+    </div>
+  );
+}
+
+export default withRoleProtection(PatientsPage, ["DOCTOR", "ADMIN"]);
