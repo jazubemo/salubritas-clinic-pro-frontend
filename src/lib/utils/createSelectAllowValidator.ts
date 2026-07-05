8581923fa1042a5a27f511f14f5374b28efa2a54
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-07-04 18:15:09 -0600

    feat: block users to create past events

--- src/lib/utils/createSelectAllowValidator.ts
+++ src/lib/utils/createSelectAllowValidator.ts
@@ -8,15 +8,26 @@ import { EventImpl } from "@fullcalendar/core/internal";
  */
 export const createSelectAllowValidator = (appointments: AppointmentEvent[]) => {
   return (span: DateSpanApi, movingEvent: EventImpl | null): boolean => {
-    const selectionStartStr = span.startStr;
-    const selectionEndStr = span.endStr;
+    if (!span.start || !span.end) return true;
+
+    const selectionStart = span.start.getTime();
+    const selectionEnd = span.end.getTime();
+    
+    const now = new Date();
+    now.setSeconds(0, 0);
+    const timeThreshold = now.getTime();
+
+    // Block new selections if the target slot has already passed
+    if (selectionStart < timeThreshold) {
+      return false; 
+    }
 
     const isOverlapping = appointments.some((appointment) => {
       const eventStart = new Date(appointment.start).toISOString();
       const eventEnd = new Date(appointment.end).toISOString();
 
-      const selectionStartISO = new Date(selectionStartStr).toISOString();
-      const selectionEndISO = new Date(selectionEndStr).toISOString();
+      const selectionStartISO = new Date(selectionStart).toISOString();
+      const selectionEndISO = new Date(selectionEnd).toISOString();
 
       return selectionStartISO < eventEnd && selectionEndISO > eventStart;
     });
