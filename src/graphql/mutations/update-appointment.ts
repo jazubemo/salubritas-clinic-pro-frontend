bca041dff3f862c2e4a3ab3d153d2d17b27b1751
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-07-10 13:44:26 -0600

    feat: add update-appointment endpoint call

---
+++ src/graphql/mutations/update-appointment.ts
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_APPOINTMENT_QUERY = gql`
+  mutation UpdateAppointment($activeClinicId: String!, $updateAppointmentId: ID!, $updateAppointmentInput: UpdateAppointmentInput!) {
+  updateAppointment(activeClinicId: $activeClinicId, id: $updateAppointmentId, updateAppointmentInput: $updateAppointmentInput) {
+    _id
+    clinicId
+    createdAt
+    doctorId
+    doctorName
+    endTime
+    isNewPatient
+    patientId
+    patientName
+    reason
+    startTime
+    status
+    updatedAt
+  }
+}
+`;
